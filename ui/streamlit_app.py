bccc509c96828357297ebc8525d512f57a1be5cb
noobshubham <noobshubham65@gmail.com>
2026-01-04 20:39:38 +0530

    u: API_URL for ui/streamlit_app.py

--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 st.set_page_config(page_title="Car Price Prediction", page_icon="🚗", layout="centered")
 
 API_URL = (
-    "https://car-prediction-lpfl.onrender.com/predict"
+    "https://carvalue-predictor.onrender.com/predict"
     or "http://127.0.0.1:8000/predict"
 )  # change if your endpoint differs
 
