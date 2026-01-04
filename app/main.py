81f2743a46abfb7d8f1b20e3c21dcfc256ffbec3
noobshubham <noobshubham65@gmail.com>
2026-01-04 20:28:42 +0530

    u: added CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from app.schema import CarFeatures, PredictionResponse
 from ml.model import predict_price, load_artifacts
 
 app = FastAPI(title="Car Price Prediction API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.on_event("startup")
 def startup_event():
